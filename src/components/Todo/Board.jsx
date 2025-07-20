ba773a9dd0c2d4294a34c6251f0816d26459c63e
salehibneomar <salehibneomar@gmail.com>
2025-07-21 02:17:55 +0600

    bugfix/duplicate-key

--- src/components/Todo/Board.jsx
+++ src/components/Todo/Board.jsx
@@ -13,7 +13,7 @@ const TodoBoard = props => {
 		<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
 			{todos.map((todo, index) => (
 				<TodoCard
-					key={todo.id + index}
+					key={`${todo.id}-${index}`}
 					todo={todo}
 					onDelete={handleOnDelete}
 					onView={handleOnView}
